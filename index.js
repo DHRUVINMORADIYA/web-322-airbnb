43838eea919f814db1b0ea80c8dec32f3e61670c
Dhruvin Moradiya <mdhruvin946@gmail.com>
2021-07-02 20:48:23 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const app = express();
+
+app.engine(
+  "hbs",
+  exphbs({
+    defaultLayout: "main",
+    extname: ".hbs",
+    helpers: {
+      todaysDate: (date) => new Date(date),
+    },
+  })
+);
+
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/listings", (req, res) => {
+  const listingData = [
+    {
+      id: "1",
+      title: "one bed one bath",
+      description: "some more info",
+      price: "100",
+      image: `https://picsum.photos/200`,
+    },
+    {
+      id: "2",
+      title: "two bed one bath",
+      description: "some more info",
+      price: "20",
+      image: `https://picsum.photos/200`,
+    },
+    {
+      id: "3",
+      title: "no bed no bath",
+      description: "some more info",
+      price: "40",
+      image: `https://picsum.photos/200`,
+    },
+    {
+      id: "4",
+      title: "100 bed no bath",
+      description: "some more info",
+      price: "10",
+      image: `https://picsum.photos/200`,
+    },
+  ];
+  res.render("listings", { listingData: listingData });
+});
+
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.listen(8080, () => {
+  console.log("The web server has started on port 8080");
+});

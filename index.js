b8a33759643ee1b6f2493855208555893b9c6c3f
Dhruvin Moradiya <mdhruvin946@gmail.com>
2021-07-04 12:05:12 -0400

    n

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const app = express();
 
-const PORT = process.env.port || "8080";
+const PORT = process.env.port || 8080;
 
 app.engine(
   "hbs",

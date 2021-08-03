8ce772d5708a88208d83a06ac609a609963fa976
Dhruvin Moradiya <mdhruvin946@gmail.com>
2021-08-03 10:21:35 -0400

    'm'

--- model/connection.js
+++ model/connection.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const URI = process.env.MONGODB_URI;
 
 const connectDB = async () => {
-  await mongoose.connect(URI || "mongodb://localhost:27017/TodoApp", {
+  await mongoose.connect(URI, {
     useNewUrlParser: true,
   });
   console.log("db connected..!");
